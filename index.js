08fa91c67a232ebef63c8cd136d4b8b2ae49035d
加缪 <jiamiu.hzy@alibaba-inc.com>
2014-10-17 16:42:34 +0800

    standard all modules to npm

--- index.js
+++ index.js
@@ -1,60 +1,55 @@
-var config = require('./config')
-
-
-var userModule = {
-  models : require('./models'),
-  listen : require('./listen')(config),
-  //this will allow app global config overwrite
-  config : config,
-  route : {
-    "/user/count" : function( req, res, next){
-      userModule.dep.model.models['user'].count().then(function(total){
-        res.json({count:total})
-      })
-    },
-    "*" : {
-      "function" : function initSession(req,res,next){
-        //TODO only for dev
-        if( !req.session.user ){
-          userModule.dep.model.models['user'].count().then(function(total){
-            var skip = parseInt( total * Math.random())
-            userModule.dep.model.models['user'].find({limit:1,skip:skip}).then(function(users){
-//              console.log("====================setting session user===========", users[0].name)
-//              req.session.user = users[0]
-              next()
-            }).catch(function(err){
-              ZERO.error(err)
-              next()
-            })
-          })
-        }else{
-          next()
-        }
+var qiniu = require('qiniu')
+var fs = require("fs")
+var request = require('request')
+var base64_encode = require('base64').encode;
+var Buffer = require('buffer').Buffer;
+var uuid = require('node-uuid')
 
 
 
+function uptoken( config) {
+  qiniu.conf.ACCESS_KEY =config.accessKey;
+  qiniu.conf.SECRET_KEY = config.secretKey;
+  var putPolicy = new qiniu.rs.PutPolicy(config.bucket);
+  //putPolicy.callbackUrl = callbackUrl;
+  //putPolicy.callbackBody = callbackBody;
+  //putPolicy.returnUrl = returnUrl;
+  //putPolicy.returnBody = returnBody;
+  //putPolicy.asyncOps = asyncOps;
+  //putPolicy.expires = expires;
+  putPolicy.persistentOps= ['avthumb/webm/an/1','vsample/jpg/ss/1/t/1/pattern/'+base64_encode(new Buffer(uuid.v4()+"-$(count)"))].join(";");
+  putPolicy.persistentPipeline = config.persistentPipeLine;//"twentyone"
 
-        return
-
-//        if( req.session.user ){
-//          next()
-//        }else{
-//          //TODO only for dev
-//          userModule.dep.model.models['user'].find({limit:1}).then(function(users){
-//            req.session.user = users[0]
-//            next()
-//          }).catch(function(err){
-//            ZERO.error(err)
-//            next()
-//          })
-//        }
-
-      },
-      "order" : {first:true}
-    }
-
-  }
+  return putPolicy.token();
 }
 
-module.exports = userModule
+module.exports = {
+  route : {
+    "GET /qiniu/status/:pid" : function( req, res){
+      request('http://api.qiniu.com/status/get/prefop?id='+req.param('pid'),function( err, respond, body){
+        console.log( err, body )
+        res.json(JSON.parse(body))
+      })
+    }
+  },
+  reliers : {},
+  expand : function( module ){
+    var root = this
+    if( module.qiniu ){
+      root.reliers[module.name] = module.qiniu
+      var route = root.uptokenRoute( module.name)
 
+      root.dep.request.add( route.url, route.handler )
+    }
+  },
+  uptokenRoute : function( name ){
+    var root = this
+
+    return {
+      url: "GET /qiniu/uptoken/" + name,
+      handler: function (req, res) {
+        res.json({uptoken:uptoken(root.reliers[name])})
+      }
+    }
+  }
+}
